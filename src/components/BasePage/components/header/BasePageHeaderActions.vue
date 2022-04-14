7ea88ec3f4a21ab2de3a832a5ac4121730b2d8c5
fredattack <Fred01072015!>
2022-04-14 12:59:00 +0200

    remove git from gitignore

--- src/components/BasePage/components/header/BasePageHeaderActions.vue
+++ src/components/BasePage/components/header/BasePageHeaderActions.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex flex-col md:flex-row flex-1 justify-end mt-4 sm:mt-0">
+  <div class="flex flex-row md:flex-row flex-1 justify-end mt-4 sm:mt-0">
     <slot></slot>
   </div>
 </template>
