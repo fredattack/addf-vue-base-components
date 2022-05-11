d6f7bb5b2620ae7d82ff815de8533741cff0d623
MickyCompanie <micky.companie@gmail.com>
2022-05-11 13:41:06 +0200

    BaseSimpleTab

--- dist/index.js
+++ dist/index.js
@@ -5809,7 +5809,7 @@ function render$w(_ctx, _cache, $props, $setup, $data, $options) {
                     href: tab.href,
                     onClick: $event => (this.$router.push({name: tab.routeName})),
                     class: vue.normalizeClass([this.$route.name === tab.routeName ? `border-theme-${$props.colorWhenSelected} text-theme-${$props.colorWhenSelected}` :
-             'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm']),
+             'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'group inline-flex items-center py-4 px-1 border-b-2 font-medium md:text-sm sm:text-xs']),
                     "aria-current": tab.current ? 'page' : undefined
                   }, [
                     (tab.icon)
--- src/components/BaseTabs/BaseSimpleTab.vue
+++ src/components/BaseTabs/BaseSimpleTab.vue
@@ -6,7 +6,7 @@
         <nav class="-mb-px flex space-x-8 cursor-pointer" aria-label="Tabs">
           <template v-for="tab in tabs" :key="tab.label" >
             <a v-if='tab.gate !== false' :href="tab.href" @click="this.$router.push({name: tab.routeName})" :class="[this.$route.name === tab.routeName ? `border-theme-${colorWhenSelected} text-theme-${colorWhenSelected}` :
-             'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'group inline-flex items-center py-4 px-1 border-b-2 font-medium text-sm']"
+             'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'group inline-flex items-center py-4 px-1 border-b-2 font-medium md:text-sm sm:text-xs']"
                :aria-current="tab.current ? 'page' : undefined">
 
               <font-awesome-icon v-if='tab.icon' :icon="['fal', tab.icon]" class='mr-2'/>
