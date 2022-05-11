678858cc22526cf9407e72377a4284fd6ae9fe69
MickyCompanie <micky.companie@gmail.com>
2022-05-11 13:57:03 +0200

    BaseSimpleTab

--- src/components/BaseTabs/BaseSimpleTab.vue
+++ src/components/BaseTabs/BaseSimpleTab.vue
@@ -1,9 +1,9 @@
 <template>
   <div v-show='cVisibility'>
 
-    <div class="md:block sm:block">
+    <div class="block">
       <div class="border-b border-gray-200">
-        <nav class="-mb-px md:flex sm:grid sm:grid-cols-5 space-x-8 cursor-pointer" aria-label="Tabs">
+        <nav class="-mb-px md:flex sm:grid sm:grid-cols-1 space-x-8 cursor-pointer" aria-label="Tabs">
           <template v-for="tab in tabs" :key="tab.label" >
             <a v-if='tab.gate !== false' :href="tab.href" @click="this.$router.push({name: tab.routeName})" :class="[this.$route.name === tab.routeName ? `border-theme-${colorWhenSelected} text-theme-${colorWhenSelected}` :
              'border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300', 'group inline-flex items-center py-4 px-1 border-b-2 font-medium md:text-sm sm:text-xs']"
