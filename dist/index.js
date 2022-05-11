5b77c442c7c93271c9b19f2af45c0c9a3762fee2
MickyCompanie <micky.companie@gmail.com>
2022-05-11 14:13:19 +0200

    BaseSimpleTab

--- dist/index.js
+++ dist/index.js
@@ -5786,7 +5786,7 @@ var script$w = {
 const _hoisted_1$l = { class: "block" };
 const _hoisted_2$h = { class: "border-b border-gray-200" };
 const _hoisted_3$d = {
-  class: "-mb-px md:flex sm:grid sm:grid-cols-1 md:space-x-8 sm:space-x-3 cursor-pointer",
+  class: "-mb-px md:flex sm:grid sm:grid-cols-2 md:space-x-8 sm:space-x-3 cursor-pointer",
   "aria-label": "Tabs"
 };
 const _hoisted_4$9 = { class: "capitalize-first" };
